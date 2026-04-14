69ff989716edac34ace9499af23fac483973c14b
bsun <Drbethsun@gmail.com>
2026-04-13 20:06:20 -0400

    Create App.jsx

---
+++ frontend/scr/App.jsx
@@ -0,0 +1,11 @@
+import ARCanvas from "./components/ARCanvas";
+import ProductPanel from "./components/ProductPanel";
+
+export default function App() {
+  return (
+    <div>
+      <ARCanvas />
+      <ProductPanel />
+    </div>
+  );
+}
