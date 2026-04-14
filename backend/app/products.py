3c55bc649f84f9c27c3c0c2d810697f42a29451f
bsun <Drbethsun@gmail.com>
2026-04-13 20:12:34 -0400

    Create products.py

---
+++ backend/app/products.py
@@ -0,0 +1,16 @@
+PRODUCTS = [
+    {
+        "id": "mac_rubywoo",
+        "name": "MAC Ruby Woo",
+        "type": "lipstick",
+        "hex": "#a50021",
+        "url": "https://sephora.com"
+    },
+    {
+        "id": "fenty_130",
+        "name": "Fenty 130",
+        "type": "foundation",
+        "hex": "#f1c27d",
+        "url": "https://sephora.com"
+    }
+]
