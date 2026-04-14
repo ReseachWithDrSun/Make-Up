fa52f0896bec30c77a2ecea848bba15182813534
bsun <Drbethsun@gmail.com>
2026-04-14 15:27:27 -0400

    Create Studio.jsx

---
+++ frontend/scr/PAGES/Studio.jsx
@@ -0,0 +1,11 @@
+import ARCanvas from "../ar/ARCanvas";
+import ProductPanel from "../components/ProductPanel";
+
+export default function Studio() {
+  return (
+    <>
+      <ARCanvas />
+      <ProductPanel />
+    </>
+  );
+}
