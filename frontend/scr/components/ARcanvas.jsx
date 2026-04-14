1fe9abade4a41e47720f59e0a0901a6602f9dba1
bsun <Drbethsun@gmail.com>
2026-04-13 20:10:26 -0400

    Create ARcanvas.jsx

---
+++ frontend/scr/components/ARcanvas.jsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+
+export default function ARCanvas() {
+  useEffect(() => {
+    // paste your FULL main.js code here
+    // (the upgraded version with segmentation + shaders)
+  }, []);
+
+  return null;
+}
